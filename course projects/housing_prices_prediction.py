7e789cb579d80dd77f21d0506277b69e495f8d6f
Glen Dsilva <glenadsilva07@gmail.com>
2025-02-02 16:17:26 +0530

    Added Random Forest model

--- course projects/housing_prices_prediction.py
+++ course projects/housing_prices_prediction.py
@@ -1,5 +1,6 @@
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
 import pandas as pd
 
@@ -13,7 +14,16 @@ X_train,X_test,y_train,y_test = train_test_split(X,y)
 model = DecisionTreeRegressor(random_state=1)
 model.fit(X_train,y_train)
 predictions = model.predict(X_test)
-print(f"Predictions of firt 5 data: {predictions[:5]}")
+print(f"Predictions of firt 5 data using Decision Tree model: {predictions[:5]}")
 print(f"Actual values: {list(y_test)[:5]}")
 mse = mean_absolute_error(predictions,y_test)
-print(f"mean absolute error: {mse}")
+print(f"mean absolute error for Decision Tree Model: {mse}")
+
+
+model_2 = RandomForestRegressor(random_state=1)
+model_2.fit(X_train,y_train)
+predictions = model_2.predict(X_test)
+print(f"Predictions of first 5 data using Random Forest model: {predictions[:5]}")
+print(f"Actual values: {list(y_test)[:5]}")
+mse = mean_absolute_error(predictions, y_test)
+print(f"mean absolute error for Random Forest model: {mse}")
