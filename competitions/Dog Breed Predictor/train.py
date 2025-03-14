11bed86fd2d56715fa0880174be136501cfc0012
Glen Dsilva <glenadsilva07@gmail.com>
2025-03-14 21:10:01 +0530

    Added EarlyStopping and LR Scheduler

--- competitions/Dog Breed Predictor/train.py
+++ competitions/Dog Breed Predictor/train.py
@@ -42,27 +42,6 @@ transform = transforms.Compose([
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 ])
 
-train_img_dir = "Data\\train"
-test_img_dir = "Data\\test"
-csv_file_path = "Data\\labels.csv"
-train_dataset = CustomImageDataset(img_dir=train_img_dir,csv_file=csv_file_path,transform=transform)
-dataset_size = len(train_dataset)
-indices = list(range(dataset_size))
-split = int(0.1*dataset_size)
-np.random.shuffle(indices)
-train_indices, val_indices = indices[split:], indices[:split]
-train_sampler = SubsetRandomSampler(train_indices)
-val_sampler = SubsetRandomSampler(val_indices)
-train_loader = DataLoader(train_dataset,batch_size=32,sampler=train_sampler)
-val_loader = DataLoader(train_dataset,batch_size=32,sampler=val_sampler)
-test_dataset = CustomImageDataset(img_dir=test_img_dir, transform=transform)
-test_loader = DataLoader(test_dataset,batch_size=32,shuffle=True)
-# image, labels = next(iter(train_loader))
-# plt.imshow(image[0].permute(1, 2, 0))
-# plt.show()
-df = pd.read_csv(csv_file_path)
-num_classes = df["breed"].nunique()
-# print(num_classes)
 
 class CustomResNet(nn.Module):
     def __init__(self,num_classes):
@@ -109,48 +88,101 @@ class CustomResNet(nn.Module):
 # model = CustomResNet(num_classes)
 # print(model)
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-net = CustomResNet(num_classes).to(device)
-summary(model=net,
-        input_size=(32,3,224,224),
-        col_names=["input_size","output_size","num_params","trainable"],
-        col_width=20,
-        row_settings=["var_names"]
-)
-loss_fn = nn.CrossEntropyLoss()
-optimizer = optim.Adam(net.fc.parameters(),lr=0.0001)
-# scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.1)
 
-NUM_EPOCHS = 10
-print(f"Training on Device: {device} ...")
-for epoch in range(NUM_EPOCHS):
-    running_loss = 0.0
-    running_acc = 0.0
-    net.train()
-    for i, (images, labels) in enumerate(train_loader):
-        print(f"running batch {i+1}",end=", ")
-        images, labels = images.to(device), labels.to(device)
-        optimizer.zero_grad()
-        outputs = net(images)
-        loss = loss_fn(outputs, labels)
-        loss.backward()
-        optimizer.step()
-        running_loss += loss.item()
-        pred_class = torch.argmax(outputs, dim=1)
-        running_acc += (pred_class == labels).sum().item()/len(outputs)
-        print(f"running loss : {running_loss:.4f} ",end="\r",flush=True)
-    print(f"Epoch {epoch+1}, loss: {running_loss/len(train_loader):.4f}, acc: {(running_acc/len(train_loader))*100:.2f}%",end=", ")
-    # scheduler.step()
-    net.eval()
-    val_loss, val_acc = 0,0
-    with torch.no_grad():
-        for images,labels in val_loader:
-            images,labels = images.to(device),labels.to(device)
-            val_outputs = net(images)
-            loss = loss_fn(val_outputs,labels)
-            val_loss += loss.item()
-            pred_class = torch.argmax(val_outputs, dim=1)
-            val_acc += (pred_class == labels).sum().item()/len(val_outputs)
-        print(f"val_loss: {val_loss/len(val_loader):.4f}, val_acc: {(val_acc/len(val_loader))*100:.2f}%")
-print("Training Complete")
+class EarlyStopper:
+    def __init__(self, patience=1, min_delta=0):
+        self.patience = patience
+        self.min_delta = min_delta
+        self.counter = 0
+        self.min_validation_loss = float('inf')
 
+    def early_stop(self, validation_loss):
+        if validation_loss < self.min_validation_loss:
+            self.min_validation_loss = validation_loss
+            self.counter = 0
+        elif validation_loss > (self.min_validation_loss + self.min_delta):
+            self.counter += 1
+            if self.counter >= self.patience:
+                return True
+        return False
+
+
+if __name__ == "__main__":
+    train_img_dir = "Data\\train"
+    test_img_dir = "Data\\test"
+    csv_file_path = "Data\\labels.csv"
+    train_dataset = CustomImageDataset(img_dir=train_img_dir,csv_file=csv_file_path,transform=transform)
+    dataset_size = len(train_dataset)
+    indices = list(range(dataset_size))
+    split = int(0.1*dataset_size)
+    np.random.shuffle(indices)
+    train_indices, val_indices = indices[split:], indices[:split]
+    train_sampler = SubsetRandomSampler(train_indices)
+    val_sampler = SubsetRandomSampler(val_indices)
+    train_loader = DataLoader(train_dataset,batch_size=32,sampler=train_sampler)
+    val_loader = DataLoader(train_dataset,batch_size=32,sampler=val_sampler)
+    test_dataset = CustomImageDataset(img_dir=test_img_dir, transform=transform)
+    test_loader = DataLoader(test_dataset,batch_size=32,shuffle=True)
+    # image, labels = next(iter(train_loader))
+    # plt.imshow(image[0].permute(1, 2, 0))
+    # plt.show()
+    df = pd.read_csv(csv_file_path)
+    num_classes = df["breed"].nunique()
+    # print(num_classes)
+
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    net = CustomResNet(num_classes).to(device)
+    summary(model=net,
+            input_size=(32,3,224,224),
+            col_names=["input_size","output_size","num_params","trainable"],
+            col_width=20,
+            row_settings=["var_names"]
+    )
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(net.fc.parameters(),lr=0.0001)
+    # scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.1)
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=3)
+
+
+    early_stopping = EarlyStopper(patience=3, min_delta=0.01)
+
+    NUM_EPOCHS = 30
+    print(f"Training on device {device} for {NUM_EPOCHS} epochs")
+    for epoch in range(NUM_EPOCHS):
+        running_loss = 0.0
+        running_acc = 0.0
+        net.train()
+        for i, (images, labels) in enumerate(train_loader):
+            print(f"training on batch {i+1}",end=", ")
+            images, labels = images.to(device), labels.to(device)
+            optimizer.zero_grad()
+            outputs = net(images)
+            loss = loss_fn(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+            pred_class = torch.argmax(outputs, dim=1)
+            running_acc += (pred_class == labels).sum().item()/len(outputs)
+            print(f"running loss : {running_loss:.4f} ",end="\r",flush=True)
+        print(f"Epoch {epoch+1}, loss: {running_loss/len(train_loader):.4f}, acc: {(running_acc/len(train_loader))*100:.2f}%",end=", ")
+        # scheduler.step()
+        net.eval()
+        val_loss, val_acc = 0,0
+        with torch.no_grad():
+            for images,labels in val_loader:
+                images,labels = images.to(device),labels.to(device)
+                val_outputs = net(images)
+                loss = loss_fn(val_outputs,labels)
+                val_loss += loss.item()
+                pred_class = torch.argmax(val_outputs, dim=1)
+                val_acc += (pred_class == labels).sum().item()/len(val_outputs)
+            scheduler.step(val_loss/len(val_loader))
+            if early_stopping.early_stop(val_loss/len(val_loader)):
+                print("Early stopping triggered!")
+                break
+            print(f"val_loss: {val_loss/len(val_loader):.4f}, val_acc: {(val_acc/len(val_loader))*100:.2f}%")
+    print("Training Complete")
+    print("Saving model")
+    torch.save(net.state_dict(), "model.pth")
+    print("Model saved")
